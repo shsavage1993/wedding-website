f4e1393192d741b1658575b7fa0c984e4f871dce
Stephen Henry Savage <shsavage1993@gmail.com>
2021-12-15 02:01:07 +0800

    Modifications to master sections: master welcome and redirection to login page if not signed in for gallery and rsvp pages

--- src/pages/MasterGalleryPage.tsx
+++ src/pages/MasterGalleryPage.tsx
@@ -1,6 +1,17 @@
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { CodeContext, AuthContext } from '../App';
+import { Redirect } from 'react-router';
 import { GalleryPage } from './GalleryPage';
 
 export const MasterGalleryPage: FC = () => {
-	return <GalleryPage master={true} />;
+	const signedIn = useContext(AuthContext);
+	const code = useContext(CodeContext);
+
+	if (signedIn === true) {
+		return <GalleryPage master={true} />;
+	} else if (signedIn === false) {
+		return <Redirect to={`/${code}`} />;
+	} else {
+		return null;
+	}
 };
--- src/pages/MasterRsvpPage.tsx
+++ src/pages/MasterRsvpPage.tsx
@@ -1,18 +1,33 @@
 // import React from 'react';
-import { FC } from 'react';
+import { FC, useContext } from 'react';
+import { CodeContext, AuthContext } from '../App';
+import { Redirect } from 'react-router';
 import { MotionDiv } from '../components/MotionDiv';
 import Container from 'react-bootstrap/Container';
 import Col from 'react-bootstrap/Col';
 import { RsvpTable } from '../components/RsvpTable';
 
 export const MasterRsvpPage: FC = () => {
-	return (
-		<MotionDiv>
-			<Container fluid className="h-100" style={{ marginTop: '76px' }}>
-				<Col className="p-5 h-100">
-					<RsvpTable />
-				</Col>
-			</Container>
-		</MotionDiv>
-	);
+	const signedIn = useContext(AuthContext);
+	const code = useContext(CodeContext);
+
+	if (signedIn === true) {
+		return (
+			<MotionDiv>
+				<Container
+					fluid
+					className="h-100"
+					style={{ marginTop: '76px' }}
+				>
+					<Col className="p-5 h-100">
+						<RsvpTable />
+					</Col>
+				</Container>
+			</MotionDiv>
+		);
+	} else if (signedIn === false) {
+		return <Redirect to={`/${code}`} />;
+	} else {
+		return null;
+	}
 };
